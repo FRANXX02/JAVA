5746ee2305f872f2bb596f2ef5fff45274313558
陈一坤 <595097595@qq.com>
2020-07-29 23:52:51 +0800

    1

--- 7.29/src/MyLinkedList.java
+++ 7.29/src/MyLinkedList.java
@@ -18,12 +18,43 @@ class Node{
     public void setData(int data) {
         this.data = data;
     }
+
+    public Node getNext() {
+        return next;
+    }
+
+    public void setNext(Node next) {
+        this.next = next;
+    }
 }
 public class MyLinkedList {
     private Node head;
+
+    public Node getHead() {
+        return head;
+    }
+
+    public void setHead(Node head) {
+        this.head = head;
+    }
     public void addFirst(int data){
         Node node=new Node (data);
-
+        node.setData (data);
+        if (this.head == null) {
+            this.setHead (node.getNext ());
+            return;
+        }
+        if (this.head != null) {
+            node.setNext (this.head.getNext ());
+            this.head.setNext (node.getNext ());
+        }
+    }
+    public void display(){
+        Node cur =this.head;
+        while (cur.getNext ()!=null){
+            System.out.print (cur.getData ()+" ");
+            cur=cur.getNext ();
+        }
 
     }
 }
---
+++ 7.29/src/Test1.java
@@ -0,0 +1,18 @@
+/**
+ * program:
+ * description:
+ * author: 陈一坤
+ * date:
+ **/
+public class Test1 {
+
+    public static void main(String[] args) {
+        MyLinkedList myLinkedList=new MyLinkedList ();
+        myLinkedList.addFirst (10);
+        myLinkedList.addFirst (15);
+        myLinkedList.addFirst (20);
+        myLinkedList.display ();
+
+
+    }
+}
